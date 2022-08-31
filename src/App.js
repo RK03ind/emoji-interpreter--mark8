262c5c9ec9a482ea2a396430f027f32168d33940
RK03ind <gamitech95@gmail.com>
2022-08-31 15:34:47 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const [inputState, setInputState] = useState("");
+  const [meaningState, setMeaningState] = useState(
+    "Emoji meaning will appear here..."
   );
-}
+  const emojiData = {
+    "🙃": "Ironical Upside-Down Smile",
+    "😘": "Kisses",
+    "🤑": "Money Face",
+    "🤯": "Mind Bending",
+    "🤗": "Hugs",
+    "🤔": "Thinking",
+    "😨": "Scared",
+    "🥴": "Woozy",
+    "🥶": "Shivering",
+    "🤫": "Shushh",
+  };
+
+  const emojis = Object.keys(emojiData);
+
+  const inputChangeHandler = (e) => {
+    const inputValue = e.target.value;
+    setInputState(inputValue);
+    if (inputValue) {
+      if (!emojis.includes(inputValue)) {
+        return setMeaningState("Cannot recognize emoji");
+      }
+      setMeaningState(emojiData[inputValue]);
+    }
+  };
+
+  const emojiClickHandler = (e) => {
+    if (e.target.nodeName === "SPAN") {
+      setMeaningState(emojiData[e.target.innerText]);
+    }
+  };
+
+  return (
+    <main>
+      <h1>Emoji Interpreter</h1>
+      <input
+        onChange={inputChangeHandler}
+        value={inputState}
+        type="text"
+        placeholder="Enter an emoji to get meaning..."
+      />
+      {inputState && <span>{inputState}</span>}
+      <span>{meaningState}</span>
+      <div onClick={emojiClickHandler}>
+        {emojis.map((item, index) => {
+          return <span key={index + item}>{item}</span>;
+        })}
+      </div>
+    </main>
+  );
+};
 
 export default App;
